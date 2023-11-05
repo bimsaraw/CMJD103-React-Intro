2fe9723799be044b69154f1a73b873995090324e
Bimsara Walallawita <bwalallawita@gmail.com>
2023-11-05 12:06:44 +0530

    React Intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [username, setUsername] = useState(null);
+  const [counter, setCounter] = useState(0);
+
+  const handleLogin = () =>{
+    //user login process
+    setUsername("bimsara");
+  }
+
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  }
+
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        <h1>Hi {username}</h1>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+
+      <div>
+        <h1>Counter: {counter}</h1>
+        <button onClick={increaseCounter}>Increase</button>
+        <button onClick={decreaseCounter}>Decrease</button>
+      </div>
+
+
+      <SampleComponent title="Passing title from Parent" />
+      <Vehicle model="Toyota Corolla" description="A family car" />
+      <Vehicle model="Landcruiser" description="A SUV" />
+      <Vehicle model="Nissan Sunny" />
     </div>
   );
+}
+
+const Vehicle = (props) => {
+  return (
+    <div>
+      <h2>{props.model}</h2>
+      {props.description &&
+        <p>{props.description}</p>
+      }
+    </div>
+  )
+}
+
+const SampleComponent = (props) => {
+  return (
+    <h1>{props.title}</h1>
+  )
 }
 
 export default App;
